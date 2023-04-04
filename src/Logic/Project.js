6b49847659fd3fb58b1bda684f7b0b848586b275
J <59581793+daechi73@users.noreply.github.com>
2023-04-03 22:25:17 -0400

    fixed date format

--- src/Logic/Project.js
+++ src/Logic/Project.js
@@ -12,7 +12,9 @@ export default class Project {
   }
   getDueDate() {
     return `
-    ${this.dueDate.getMonth()}-${this.dueDate.getDay()}-${this.dueDate.getFullYear()}
+    ${
+      this.dueDate.getUTCMonth() + 1
+    }-${this.dueDate.getUTCDate()}-${this.dueDate.getFullYear()}
     `;
   }
   getTasks() {
