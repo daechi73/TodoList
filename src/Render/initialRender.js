261396418cf81f5114c1ff64d0ae5656a143a331
J <59581793+daechi73@users.noreply.github.com>
2023-04-03 22:50:58 -0400

    deleted unused imports

--- src/Render/initialRender.js
+++ src/Render/initialRender.js
@@ -1,10 +1,8 @@
 import header from "./PageLayout/headerRender";
 import sidebar from "./PageLayout/sidebarRender";
-import mainSection from "./PageLayout/mainSectionRender";
+import { mainSection } from "./PageLayout/mainSectionRender";
 import footer from "./PageLayout/footerRender";
 import { addProjectBox } from "./PageLayout/AddProjectsBox";
-import { addTaskBox } from "./PageLayout/AddTaskBox";
-import { informationBox } from "./PageLayout/Information";
 
 const initialRender = () => {
   header();
