4f942bdf38cfc6052bb43401fe492466c0416005
J <59581793+daechi73@users.noreply.github.com>
2023-04-03 22:50:17 -0400

    added to render project list of due todays to the main container

--- src/Render/Navbar/Today.js
+++ src/Render/Navbar/Today.js
@@ -4,11 +4,33 @@ const todayRender = () => {
   const projectContainer = document.createElement("div");
   projectContainer.classList.add("projectList");
   mainSection.textContent = "";
+  let todaysDate = new Date();
+  todaysDate =
+    todaysDate.getMonth() +
+    1 +
+    "-" +
+    todaysDate.getDate() +
+    "-" +
+    todaysDate.getFullYear();
   Projects.getProjects().forEach((p) => {
-    const projectDiv = document.createElement("div");
-    projectDiv.classList.add("project");
-    projectDiv.textContent = p.name;
-    projectContainer.appendChild(projectDiv);
+    if (p.getDueDate() == todaysDate) {
+      const projectDiv = document.createElement("div");
+      const pNameDiv = document.createElement("div");
+      const pDueDateDiv = document.createElement("div");
+
+      projectDiv.classList.add("project");
+      pNameDiv.classList.add("projectName");
+      pNameDiv.textContent = p.getName();
+
+      pDueDateDiv.classList.add("pDueDateDiv");
+      pDueDateDiv.textContent = `Due Date: ${p.getDueDate()}`;
+
+      projectDiv.appendChild(pNameDiv);
+      projectDiv.appendChild(pDueDateDiv);
+      projectContainer.appendChild(projectDiv);
+    } else {
+      console.log(p.getDueDate());
+    }
   });
 
   mainSection.appendChild(projectContainer);
