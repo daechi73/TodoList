9480af7c8feb5402be4824b7157bd0dae95a3312
J <59581793+daechi73@users.noreply.github.com>
2023-04-07 17:27:35 -0400

    styled taskBox

--- src/Render/PageLayout/AddTaskBox.js
+++ src/Render/PageLayout/AddTaskBox.js
@@ -4,12 +4,12 @@ const addTaskBox = () => {
   const tbTitle = document.createElement("div");
   const tbForm = document.createElement("div");
   const tbCloseBtn = document.createElement("button");
-  const tbErrorMsg = document.createElement("div");
+  const tbCloseBtnDiv = document.createElement("div");
 
-  tbErrorMsg.classList.add("tbErrorMsg");
   taskBox.classList.add("taskBox");
   tbHeader.classList.add("tbHeader");
   tbCloseBtn.classList.add("tbCloseBtn");
+  tbCloseBtnDiv.classList.add("tbCloseBtnDiv");
   tbTitle.classList.add("tbTitle");
   tbForm.classList.add("tbForm");
 
@@ -17,17 +17,21 @@ const addTaskBox = () => {
   tbCloseBtn.textContent = "X";
 
   tbForm.innerHTML = `
+  <tbTitleLb class="tbTitlelb">
+    <label for="taskTitleInput" class="taskTitleLb">
+      Task Name:
+    </label>
+  </tbTitleLb>
   <div class="taskTitleDiv">
-  <label for="taskTitleInput" class="taskTitleLb">
-    Task Name:
-  </label>
-  <input type="text" id="taskTitleInput" class="taskTitleInput" />
-</div>
+    <input type="text" id="taskTitleInput" class="taskTitleInput" />
+  </div>
 
-<div class="taskPriorityDiv">
-  <label for="taskPriorityInput" class="taskPriorityLb">
-  Priority:
-  </label>
+  <div class="tbPriorityLb">
+    <label for="taskPriorityInput" class="taskPriorityLb">
+      Priority:
+    </label>
+  </div>
+  <div class="taskPriorityDiv">  
   <select id="taskPriorityInput">
   <option value="minor">Minor</option>
   <option value="medium">medium</option>
@@ -35,16 +39,17 @@ const addTaskBox = () => {
   <option value="extreme">extreme</option>
 </select>
 </div>
-
+<div class="tbErrorMsg"></div>
 <div class="taskAddBtnDiv">
   <button class="taskAddBtn" type="button">+</button>
 </div>
   `;
+
+  tbCloseBtnDiv.appendChild(tbCloseBtn);
   tbHeader.appendChild(tbTitle);
-  tbHeader.appendChild(tbCloseBtn);
+  tbHeader.appendChild(tbCloseBtnDiv);
 
   taskBox.appendChild(tbHeader);
-  taskBox.appendChild(tbErrorMsg);
   taskBox.appendChild(tbForm);
   document.querySelector(".container-main").appendChild(taskBox);
 };
--- src/Render/initialRender.js
+++ src/Render/initialRender.js
@@ -3,6 +3,7 @@ import sidebar from "./PageLayout/sidebarRender";
 import { mainSection } from "./PageLayout/mainSectionRender";
 import footer from "./PageLayout/footerRender";
 import { addProjectBox } from "./PageLayout/AddProjectsBox";
+import { addTaskBox } from "./PageLayout/AddTaskBox";
 
 const initialRender = () => {
   header();
@@ -10,6 +11,7 @@ const initialRender = () => {
   mainSection();
   footer();
   addProjectBox();
+  //addTaskBox();
 };
 
 export default initialRender;
